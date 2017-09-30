c04a8c7cc54fedc51e256f967be91d45cb7e747a
Neil Okamoto <neil.okamoto@gmail.com>
2017-09-30 14:22:26 -0700

    configure lpr for printing at home

--- init.el
+++ init.el
@@ -96,11 +96,25 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Other packages, not related to clojure development
 
-;; adjust exec-path as necessary because OSX window system launch
-;; doesn't respect your shell config
+;; adjust exec-path
+;; necessary because OSX window system launch doesn't use your shell config
+;; also make sure PATH also matches, for eshell
 (add-to-list 'exec-path "/usr/local/bin")
-;; make sure PATH also matches, for eshell
 (setenv "PATH" (mapconcat 'identity exec-path ":"))
+
+;; Print settings
+;; When changing the font size you also need to adjust the pagination
+;; as follows:
+;;   if "lpr-headers-switches" is non-nil, the flags are passed directly to lpr
+;;   if "lpr-headers-switches" is nil, then pagination is done by pr
+;;   you can pass flags to pr by setting "lpr-page-header-switches"
+(setq lpr-switches '("-o" "cpi=14"          ; characters per inch
+                     "-o" "lpi=8"           ; lines per inch
+                     "-o" "page-top=32"     ; margins in points (1/72 inch)
+                     "-o" "page-bottom=32"
+                     "-o" "page-left=32"
+                     "-o" "page-right=32"))
+(setq lpr-page-header-switches '("-l" "80") ; lines per page
 
 ;; org-mode export to markdown 
 (eval-after-load "org"
