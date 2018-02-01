abd80cb2251e15a8a5a3194cdf15136938e28852
Neil Okamoto <neil.okamoto@gmail.com>
2018-02-01 10:09:57 -0800

    optimize eshell configuration

--- custom/neo-shell.el
+++ custom/neo-shell.el
@@ -1,28 +1,26 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; INTERACTIVE SHELL
 
-(use-package virtualenvwrapper
-  :ensure t
-  :pin melpa-stable
-  :config
-  ;; where virtualenv dirs are expected to be
-  (setq venv-location "~/python-venv/")
-
-  ;; make eshell aware of virtualenv
-  (venv-initialize-eshell)
-  (setq eshell-prompt-function
-        (lambda nil
-          (concat (if venv-current-name
-                      (concat "(" venv-current-name ") "))
-                  (eshell/pwd) " $ "))))
-
 (use-package eshell
+  :bind (("C-c M-e" . eshell))
   :config
   ;; tell eshell about commands that use pager
-  (setq eshell-visual-options '(("aws" "help")))
+  (setq eshell-visual-options '(("aws" "help")
+                                ("git" "--help" "--paginate")))
+  (setq eshell-visual-subcommands '(("git" "log" "diff" "show")))
 
-  ;; always have an eshell running
-  (add-hook 'emacs-startup-hook
-            (lambda ()
-              (cd default-directory)
-              (eshell))))
+  ;; python venv needed only in eshell
+  (use-package virtualenvwrapper
+    :ensure t
+    :pin melpa-stable
+    :config
+    ;; where virtualenv dirs are expected to be
+    (setq venv-location "~/python-venv/")
+    
+    ;; make eshell aware of virtualenv
+    (venv-initialize-eshell)
+    (setq eshell-prompt-function
+          (lambda nil
+            (concat (if venv-current-name
+                        (concat "(" venv-current-name ") "))
+                    (eshell/pwd) " $ ")))))
