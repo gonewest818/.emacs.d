3bec8212024df93b51a23b6c05b10ec8f3231a87
Neil Okamoto <neil.okamoto@gmail.com>
2018-02-01 12:48:42 -0800

    disable dashboard for faster startup

--- custom/neo-wip.el
+++ custom/neo-wip.el
@@ -27,24 +27,26 @@
       :bind ("C-c am" . mincal-display))))
 
 ;;; DASHBOARD: https://github.com/gonewest818/emacs-dashboard
-(defun nokamoto-dashboard-config (&rest label)
-  (setq dashboard-banner-logo-title (concat "Emacs " emacs-version
-                                            " | " (current-time-string)
-                                            (if label (concat " | " label))))
-  (setq dashboard-startup-banner (mincal-retrieve))
-  (setq dashboard-image-banner-max-width 350)
-  (setq dashboard-image-banner-max-height 350)
-  (setq dashboard-items '((recents . 5) (projects . 5) (bookmarks . 5)))
-  (dashboard-setup-startup-hook))
 
-(if (setq nokamoto-dashboard-use-dev nil) ; set true to use dev version
-    (let ((dashboard-path (concat user-emacs-directory "dev/emacs-dashboard")))
-      (when (file-directory-p dashboard-path)
-        (add-to-list 'load-path dashboard-path)
-        (use-package dashboard
-          :pin manual
-          :config (nokamoto-dashboard-config "*DEV*"))))
-  (use-package dashboard
-    :ensure t
-    :config (nokamoto-dashboard-config)))
+;; (defun nokamoto-dashboard-config (&rest label)
+;;   (setq dashboard-banner-logo-title (concat "Emacs " emacs-version
+;;                                             " | " (current-time-string)
+;;                                             (if label (concat " | " label))))
+;;   (setq dashboard-startup-banner 1)
+;;   ;;(setq dashboard-startup-banner (mincal-retrieve))
+;;   ;;(setq dashboard-image-banner-max-width 350)
+;;   ;;(setq dashboard-image-banner-max-height 350)
+;;   (setq dashboard-items '((recents . 5) (projects . 5) (bookmarks . 5)))
+;;   (dashboard-setup-startup-hook))
+
+;; (if (setq nokamoto-dashboard-use-dev nil) ; set true to use dev version
+;;     (let ((dashboard-path (concat user-emacs-directory "dev/emacs-dashboard")))
+;;       (when (file-directory-p dashboard-path)
+;;         (add-to-list 'load-path dashboard-path)
+;;         (use-package dashboard
+;;           :pin manual
+;;           :config (nokamoto-dashboard-config "*DEV*"))))
+;;   (use-package dashboard
+;;     :ensure t
+;;     :config (nokamoto-dashboard-config)))
 
