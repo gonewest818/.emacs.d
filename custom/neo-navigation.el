c2e7e7cbfb0198cee20e5e4a7918205ed09feb31
Neil Okamoto <neil.okamoto@gmail.com>
2018-02-01 13:01:04 -0800

    optimize load times for ivy, counsel, swiper

--- custom/neo-navigation.el
+++ custom/neo-navigation.el
@@ -17,24 +17,28 @@
 (use-package ivy
   :ensure t
   :pin melpa-stable
+  :commands (ivy-mode
+             ivy-completing-read
+             ivy-completion-in-region
+             ivy-read)
+  :bind (("C-c C-r" . ivy-resume)
+         ("C-x b"   . ivy-switch-buffer))
   :config
   (setq ivy-use-virtual-buffers t)
   (setq ivy-count-format "(%d/%d) ")
   (setq ivy-wrap t)
-  (bind-keys ("C-c C-r" . ivy-resume)
-             ("C-x b"   . ivy-switch-buffer))
-  (ivy-mode 1))
+  ;;(ivy-mode 1)
+  )
 
 (use-package counsel
   :ensure t
   :pin melpa-stable
-  :config
-  (bind-keys ("M-x"     . counsel-M-x)
-             ("C-x C-f" . counsel-find-file)
-             ("C-h f"   . counsel-describe-function)
-             ("C-h v"   . counsel-describe-variable)
-             ("C-c g"   . counsel-git)
-             ("C-c j"   . counsel-git-grep)))
+  :bind (("M-x"     . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-h f"   . counsel-describe-function)
+         ("C-h v"   . counsel-describe-variable)
+         ("C-c g"   . counsel-git)
+         ("C-c j"   . counsel-git-grep)))
 
 (use-package swiper
   :ensure t
@@ -45,6 +49,8 @@
   :ensure t
   :pin melpa-stable
   :diminish "prj"
+  :bind-keymap (("C-c p" . projectile-command-map))
   :config
   (setq projectile-completion-system 'ivy)
-  (projectile-global-mode 1))
+  ;;(projectile-global-mode 1)
+  )
