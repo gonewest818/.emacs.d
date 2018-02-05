e9db75b59e18fe6e6a637cccb576f0ad9d158107
Neil Okamoto <neil.okamoto@gmail.com>
2018-02-05 08:18:21 -0800

    drop dashboard

--- lisp/neo-wip.el
+++ lisp/neo-wip.el
@@ -1,4 +1,4 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; EMACS PACKAGES IN DEVELOPMENT
 
 ;;; DIMMER: https://github.com/gonewest818/dimmer.el
@@ -25,28 +25,4 @@
       :pin manual
       :commands (mincal-display mincal-retrieve)
       :bind ("C-c am" . mincal-display))))
-
-;;; DASHBOARD: https://github.com/gonewest818/emacs-dashboard
-
-;; (defun nokamoto-dashboard-config (&rest label)
-;;   (setq dashboard-banner-logo-title (concat "Emacs " emacs-version
-;;                                             " | " (current-time-string)
-;;                                             (if label (concat " | " label))))
-;;   (setq dashboard-startup-banner 1)
-;;   ;;(setq dashboard-startup-banner (mincal-retrieve))
-;;   ;;(setq dashboard-image-banner-max-width 350)
-;;   ;;(setq dashboard-image-banner-max-height 350)
-;;   (setq dashboard-items '((recents . 5) (projects . 5) (bookmarks . 5)))
-;;   (dashboard-setup-startup-hook))
-
-;; (if (setq nokamoto-dashboard-use-dev nil) ; set true to use dev version
-;;     (let ((dashboard-path (concat user-emacs-directory "dev/emacs-dashboard")))
-;;       (when (file-directory-p dashboard-path)
-;;         (add-to-list 'load-path dashboard-path)
-;;         (use-package dashboard
-;;           :pin manual
-;;           :config (nokamoto-dashboard-config "*DEV*"))))
-;;   (use-package dashboard
-;;     :ensure t
-;;     :config (nokamoto-dashboard-config)))
 
