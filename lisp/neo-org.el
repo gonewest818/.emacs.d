7ba19d24f03f4b178eb1c962babafc39c5304480
Neil Okamoto <neil.okamoto@gmail.com>
2020-01-14 10:11:44 -0800

    simplify org-capture templates

--- lisp/neo-org.el
+++ lisp/neo-org.el
@@ -12,27 +12,13 @@
   (setq org-capture-templates
         '(("t" "ToDo" entry
            (file+headline "~/Dropbox/org/index.org" "Unfiled Tasks")
-           "* TODO %?\nSCHEDULED: %t\n  %i\n  %a")
+           "* TODO %?\n  %t\n  %i\n  %a")
           ("s" "Shopping Item" entry
            (file+headline "~/Dropbox/org/index.org" "Shopping")
-           "* BUY %?\nSCHEDULED: %t\n")
-          ("d" "Entries with Deadlines (beorg)")
-          ("dt" "ToDo with Deadline" entry
-           (file+headline "~/Dropbox/org/index.org" "Unfiled Tasks")
-           "* TODO %?\nDEADLINE: %(org-time-stamp nil) SCHEDULED: %t\n  %i\n  %a")
-          ("ds" "Shopping Item with Deadline" entry
-           (file+headline "~/Dropbox/org/index.org" "Shopping")
-           "* BUY %?\nDEADLINE: %(org-time-stamp nil) SCHEDULED: %t\n")
-          ("dj" "Journal ToDo with Deadline" entry
-           (file+datetree "~/Dropbox/org/journal.org")
-           "* TODO %? %^g\nDEADLINE: %(org-time-stamp nil) SCHEDULED: %t\n  %i\n")
-          ("j" "Job Search Journal")
-          ("jl" "Journal Log" entry
-           (file+datetree "~/Dropbox/org/journal.org")
-           "* %? %^g\n  LOGGED: %U\n  %i\n")
-          ("jt" "Journal ToDo" entry
-           (file+datetree "~/Dropbox/org/journal.org")
-           "* TODO %? %^g\nSCHEDULED: %t\n  %i\n"))))
+           "* BUY %?\n  %t\n")
+          ("j" "Journal ToDo" entry
+           (file+olp+datetree "~/Dropbox/org/journal.org")
+           "* TODO %? %^g\n  %t\n  %i\n"))))
 
 (use-package ox-md
   :after ox)
