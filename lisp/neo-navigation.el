8cd0a5d698a0bd253d93c84bcd71a9b22af2db1f
Neil Okamoto <neil.okamoto@gmail.com>
2020-02-22 17:04:51 -0800

    setup dumb-jump with ivy and ripgrep

--- lisp/neo-navigation.el
+++ lisp/neo-navigation.el
@@ -76,6 +76,18 @@
           (ibuffer-update nil t)))))
   (add-hook 'ibuffer-hook #'neo-set-filter-groups))
 
+(use-package dumb-jump
+  :ensure t
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g b" . dumb-jump-back)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config
+  (setq dumb-jump-selector 'ivy)
+  (setq dumb-jump-prefer-searcher 'rg))
+
 ;; when visiting emacs source buffers, make them read-only
 (dir-locals-set-class-variables
  'emacs-sources
