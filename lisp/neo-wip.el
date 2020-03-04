c64cc915aef387d2ff89ce751506babb61be7cb0
Neil Okamoto <neil.okamoto@gmail.com>
2020-03-03 16:31:17 -0800

    neo-open-map-at-location (on darwin)

--- lisp/neo-wip.el
+++ lisp/neo-wip.el
@@ -7,8 +7,25 @@
 (use-package request-deferred
   :ensure t)
 
+(defun neo-open-map-at-location ()
+  "Open map at coordinates found in `geolocation-location'."
+  (interactive)
+  (when geolocation-location
+    (let ((display-buffer-alist
+           (cons (cons "\\*Async Shell Command\\*.*"
+                       (cons #'display-buffer-no-window nil))
+                 display-buffer-alist)))
+      (message (format-time-string
+                "open map: %D %r"
+                (seconds-to-time (alist-get 'timestamp geolocation-location))))
+      (async-shell-command
+       (format "open \"https://maps.apple.com/?q=%f,%f\""
+               (alist-get 'latitude geolocation-location)
+               (alist-get 'longitude geolocation-location))))))
+
 (use-package geolocation
   :load-path "~/.emacs.d/dev/geolocation"
+  :bind (("C-c ql" . neo-open-map-at-location))
   :commands (geolocation-update-position
              geolocation-get-position
              geolocation-scan-wifi))
