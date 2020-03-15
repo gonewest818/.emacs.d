bd0ef949a1be1626509b9ab41ae89f9fa725c895
Neil Okamoto <neil.okamoto@gmail.com>
2020-03-15 12:55:02 -0700

    bufler groups definition

--- lisp/neo-wip.el
+++ lisp/neo-wip.el
@@ -12,13 +12,83 @@
 (use-package bufler
   :ensure t
   :demand t
-  :quelpa (bufler :fetcher github :repo "alphapapa/bufler.el")
-  ;;:quelpa (bufler :fetcher github :repo "gonewest818/bufler.el" :branch "projectile")
+  ;;:quelpa (bufler :fetcher github :repo "alphapapa/bufler.el")
+  :quelpa (bufler :fetcher github :repo "gonewest818/bufler.el" :branch "projectile-fix")
   :init
   (bind-key "C-x C-b" #'bufler)
   (bind-key "C-x b" #'bufler-workspace-switch-buffer)
-  ;; (bufler-mode t)
-  )
+  (setq bufler-groups
+        (bufler-defgroups
+          (group
+           ;; Subgroup collecting all named workspaces.
+           (auto-workspace))
+          (group
+           (group-or "Org Files"
+                     (dir (if (bound-and-true-p org-directory)
+                              org-directory
+                            "~/org"))
+                     (mode-match "org-agenda" (rx bos "org-agenda-")))
+           (group
+            ;; Subgroup collecting indirect Org buffers, grouping them by file.
+            ;; This is very useful when used with `org-tree-to-indirect-buffer'.
+            (auto-indirect)
+            (auto-file))
+           ;; Group remaining buffers by whether they're file backed, then by mode.
+           (group-not "*special*" (auto-file))
+           (auto-mode))
+          (group
+           (group-or "RSS Feeds"
+                     (name-match "elfeed" (rx bos "*elfeed-"))
+                     (name-match "eww" (rx bos "*eww"))))
+          (group
+           ;; Subgroup collecting buffers in a projectile project.
+           (auto-projectile)
+           )
+          (group
+           ;; Subgroup collecting buffers in a version-control project,
+           ;; grouping them by directory.
+           (auto-project))
+          (group
+           ;; Subgroup collecting all `help-mode' and `info-mode' buffers.
+           (group-or "*Help/Info*"
+                     (mode-match "*Help*" (rx bos "help-"))
+                     (mode-match "*Info*" (rx bos "info-"))))
+          (group
+           (group-or "*Docker*"
+                     (name-match "state"
+                                 (rx bos "*docker-"))
+                     (name-match "shells"
+                                 (rx bos "* docker run"))))
+          (group
+           ;; Subgroup collecting all special buffers (i.e. ones that are not
+           ;; file-backed), except `magit-status-mode' buffers (which are allowed to fall
+           ;; through to other groups, so they end up grouped with their project buffers).
+           (group-and "*Special*"
+                      (lambda (buffer)
+                        (unless (or (funcall (mode-match "Magit" (rx bos "magit-status"))
+                                             buffer)
+                                    (funcall (mode-match "Dired" (rx bos "dired"))
+                                             buffer)
+                                    (funcall (auto-file) buffer))
+                          "*Special*")))
+           (group
+            ;; Subgroup collecting these "special special" buffers
+            ;; separately for convenience.
+            (name-match "**Special**"
+                        (rx bos "*" (or "Messages" "Warnings" "scratch" "Backtrace") "*")))
+           (group
+            ;; Subgroup collecting all other Magit buffers, grouped by directory.
+            (mode-match "*Magit* (non-status)" (rx bos (or "magit" "forge") "-"))
+            (auto-directory))
+           ;; Subgroup for Helm buffers.
+           (mode-match "*Helm*" (rx bos "helm-"))
+           ;; Remaining special buffers are grouped automatically by mode.
+           (auto-mode))
+          ;; All buffers under "~/.emacs.d" (or wherever it is).
+          (dir user-emacs-directory)
+          ;; Group remaining buffers by directory, then major mode.
+          (auto-directory)
+          (auto-mode))))
 
 (use-package deferred
   :ensure t)
@@ -48,4 +118,3 @@ is resolved: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=39911"
   :load-path "~/.emacs.d/dev/mincal.el"
   :commands (mincal-display mincal-retrieve)
   :bind ("C-c qm" . mincal-display))
-
