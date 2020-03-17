e5d87c07a59b05f6a81d7fa8644db205395c9b4d
Neil Okamoto <neil.okamoto@gmail.com>
2020-03-17 09:34:31 -0700

    conditionally install sf511 and mincal

--- lisp/neo-wip.el
+++ lisp/neo-wip.el
@@ -102,25 +102,29 @@
 (use-package request-deferred
   :ensure t)
 
-(defun tablist--patch-wisent-total-conflicts (fn &rest args)
-  "Monkey patch `wisent-total-conflicts' to work around errors in 27.0.90.
-Supply a fictitious `load-file-name' to satisfy the code, and
-then call FN with ARGS.  This patch can be removed when the bug
-is resolved: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=39911"
-  (if (wisent-source)
-      (apply fn args)
-    (let ((load-file-name "tablist-fictitious-file.el"))
-      (apply fn args))))
+;; monkey patch not needed in emacs-27 builds *newer* than 27.0.90
 
-(unless (version< "27" emacs-version)
-  (advice-add 'wisent-total-conflicts
-              :around #'tablist--patch-wisent-total-conflicts))
+;; (defun tablist--patch-wisent-total-conflicts (fn &rest args)
+;;   "Monkey patch `wisent-total-conflicts' to work around errors in 27.0.90.
+;; Supply a fictitious `load-file-name' to satisfy the code, and
+;; then call FN with ARGS.  This patch can be removed when the bug
+;; is resolved: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=39911"
+;;   (if (wisent-source)
+;;       (apply fn args)
+;;     (let ((load-file-name "tablist-fictitious-file.el"))
+;;       (apply fn args))))
+
+;; (unless (version< "27" emacs-version)
+;;   (advice-add 'wisent-total-conflicts
+;;               :around #'tablist--patch-wisent-total-conflicts))
 
 (use-package sf511
+  :if (file-exists-p "~/.emacs.d/dev/sf511.el")
   :load-path "~/.emacs.d/dev/sf511.el"
   :bind (("C-c qs" . sf511-operators)))
 
 (use-package mincal
+  :if (file-exists-p "~/.emacs.d/dev/mincal.el")
   :load-path "~/.emacs.d/dev/mincal.el"
   :commands (mincal-display mincal-retrieve)
   :bind ("C-c qm" . mincal-display))
